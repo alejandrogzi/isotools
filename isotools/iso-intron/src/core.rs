568a25713de3748f5bc7e1c34d0f2a47acda2153
alejandrogzi <alejandrxgzi@gmail.com>
2024-11-28 01:31:18 +0100

    BREAKING CHANGE: recovery algorithm + par json descriptor (BACKUP)

--- isotools/iso-intron/src/core.rs
+++ isotools/iso-intron/src/core.rs
@@ -1,10 +1,44 @@
+//! Core module for detecting intron retentions in a query set of reads
+//!
+//!
+//! expected output type: HashMap/DashMap<ReadName, RetentionDescriptor>
+//! expected output structure: ReadName -> <T> where T: ModuleDescriptor
+//!     - ReadName is the name of the read
+//!     - <T> is a struct implementing ModuleDescriptor trait
+//!
+//! expected output example:
+//!     {
+//!        "read1": {
+//!           "intron_retention": true,
+//!           "retains_rt_intron": true,
+//!           "is_retention_supported": false,
+//!           "is_retention_supported_map": [false, false],
+//!           "retention_support_ratio": [0.23, 0.4],
+//!           "exon_support_ratio": [0.65, 0.43],
+//!           "number_of_retentions": 2,
+//!           "number_of_unsupported_retentions": 2,
+//!           "number_of_support_retentions": 0,
+//!           "location_of_retention": ["chr1:1000-2000", "chr1:3000-4000"],
+//!           "retention_in_cds": [false, true],
+//!           "retention_in_utr": [true, false],
+//!         }
+//!     }
+
+use std::collections::BTreeSet;
+use std::sync::Arc;
+
 use anyhow::Result;
-use config::{get_progress_bar, write_objs, BED3, HIT, OVERLAP_CDS, OVERLAP_EXON, PASS, SCALE};
+use config::{
+    get_progress_bar, write_objs, IntronRetentionValue, ModuleDescriptor, ModuleMap, ModuleType,
+    BED3, HIT, INTRON_RETENTION_RECOVERY_THRESHOLD, OVERLAP_CDS, OVERLAP_EXON, PASS,
+    RETENTION_RATIO_THRESHOLD, SCALE,
+};
 use dashmap::DashSet;
-use hashbrown::HashSet;
-use log::info;
+use hashbrown::{HashMap, HashSet};
+use log::{info, warn};
 use packbed::{packbed, GenePred, RefGenePred};
 use rayon::prelude::*;
+use serde_json::Value;
 
 use crate::cli::Args;
 use crate::utils::{unpack_blacklist, Bed4};
@@ -61,7 +95,8 @@ pub fn detect_intron_retentions(args: Args) -> Result<()> {
         let banned = blacklist.get(chr).unwrap_or(&binding);
 
         components.into_par_iter().for_each(|comp| {
-            let (hits, pass, blocks) = process_component(comp, banned, args.plot);
+            let (hits, pass, blocks, descriptor) =
+                process_component(comp, banned, args.plot, args.recover);
 
             hits.into_iter().for_each(|hit| {
                 hit_acc.insert(hit);
@@ -96,17 +131,18 @@ pub fn detect_intron_retentions(args: Args) -> Result<()> {
 ///
 /// # Example
 /// ```rust
-/// use iso_intron::core::process_bucket;
-/// use packbed::Bed12;
+/// use iso_intron::core::process_component;
+/// use packbed::{Bed12, GenePred, RefGenePred};
 /// use hashbrown::HashSet;
 /// use std::sync::Arc;
 ///
 /// let line = "chr1\t100\t200\ttest\t0\t+\t100\t200\t0\t1\t1,1\t0,100";
-/// let bucket = vec![Bed12::parse(line, false, false).unwrap().into()];
+/// let gp = Bed12::parse(line, true, true).unwrap();
+/// let comp = (RefGenePred::from(vec![gp.clone()]), vec![gp]);
 /// let banned = HashSet::from_iter(vec![(100, 200)]);
 /// let plot = false;
 ///
-/// let rs = process_bucket(&bucket, &banned, plot);
+/// let rs = process_component(comp, &banned, plot);
 /// ```
 ///
 /// # Description
@@ -121,16 +157,40 @@ pub fn process_component(
     comp: (RefGenePred, Vec<GenePred>),
     ban: &HashSet<(u64, u64)>,
     plot: bool,
-) -> (Vec<String>, Vec<String>, Option<String>) {
+    recover: bool,
+) -> (
+    Vec<String>,
+    Vec<String>,
+    Option<String>,
+    HashMap<String, Box<dyn ModuleMap>>,
+) {
     let mut hits = Vec::new();
     let mut pass = Vec::new();
     let mut blocks = if plot { Some(String::new()) } else { None };
 
+    let mut tmp_dirt = Vec::new();
+    let mut exon_owners = BTreeSet::new();
+    let mut introns_owned = BTreeSet::new();
+
+    let mut descriptor = HashMap::new();
+
     let refs = comp.0;
     let queries = comp.1;
 
+    let comp_size = queries.len() + refs.reads.len();
+    let (mut count, totals) = (0_f32, queries.len() as f32);
+
     for query in queries {
+        let query = Arc::new(query);
         let mut hit: bool = false;
+
+        descriptor.insert(
+            query.name.clone(),
+            ModuleDescriptor::with_schema(ModuleType::IntronRetention),
+        );
+
+        let mut number_of_retentions = 0_u32;
+        let mut location_of_retentions = Vec::new();
 
         for exon in &query.exons {
             for intron in &refs.introns {
@@ -143,43 +203,308 @@ pub fn process_component(
                 }
 
                 if exon.0 < intron.0 && exon.1 > intron.1 {
-                    hit = true;
-                    if plot {
-                        match query.strand {
-                            '+' => Bed4::from(&query.chrom, intron.0, intron.1, &query.name)
-                                .send(&mut blocks.as_mut().unwrap()),
-                            '-' => Bed4::from(
-                                &query.chrom,
-                                SCALE - intron.1,
-                                SCALE - intron.0,
-                                &query.name,
-                            )
-                            .send(&mut blocks.as_mut().unwrap()),
-                            _ => panic!("Invalid strand"),
-                        }
-
-                        continue;
+                    if !hit {
+                        count += 1.0;
+                        tmp_dirt.push(query.clone());
                     }
 
-                    // implement the logic to change the name based on UTR overlaping + in-frame
+                    hit = true;
+                    exon_owners.insert((exon.0, exon.1));
+                    introns_owned.insert((intron.0, intron.1));
 
-                    break;
+                    number_of_retentions += 1;
+
+                    match query.strand {
+                        '+' => {
+                            location_of_retentions
+                                .push(format!("{}:{}-{}", query.chrom, intron.0, intron.1,));
+
+                            if plot {
+                                Bed4::from(&query.chrom, intron.0, intron.1, &query.name)
+                                    .send(&mut blocks.as_mut().unwrap())
+                            }
+                        }
+                        '-' => {
+                            location_of_retentions.push(format!(
+                                "{}:{}-{}",
+                                query.chrom,
+                                SCALE - intron.1,
+                                SCALE - intron.0,
+                            ));
+
+                            if plot {
+                                Bed4::from(
+                                    &query.chrom,
+                                    SCALE - intron.1,
+                                    SCALE - intron.0,
+                                    &query.name,
+                                )
+                                .send(&mut blocks.as_mut().unwrap())
+                            }
+                        }
+                        _ => panic!("Invalid strand"),
+                    }
+
+                    // TODO: implement the logic to change the name based on
+                    // UTR overlaping + in-frame
+
+                    continue;
                 } else {
                     continue;
                 }
             }
         }
 
+        let handle = descriptor.get_mut(&query.name).unwrap();
+
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::NumberOfRetentions),
+                Value::Number(number_of_retentions.into()),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::RetentionLocation),
+                Value::Array(
+                    location_of_retentions
+                        .into_iter()
+                        .map(Value::String)
+                        .collect(),
+                ),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::ComponentSize),
+                Value::Number(comp_size.into()),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::RefComponentSize),
+                Value::Number(comp_size.into()),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::QueryComponentSize),
+                Value::Number(comp_size.into()),
+            )
+            .ok();
+
         if hit {
             let line = query.line().to_owned();
             hits.push(line);
+
+            handle
+                .set_value(
+                    Box::new(IntronRetentionValue::IsIntronRetention),
+                    Value::Bool(true),
+                )
+                .ok();
         } else {
             let line = query.line().to_owned();
             pass.push(line);
         }
     }
 
-    (hits, pass, blocks)
+    // after classying reads, we check bucket frequencies
+    // if the number of truncated reads is greater than 50%
+    // of the total reads in the bucket, we consider the bucket
+    // to be dirty and return the reads for recovery if args.recover
+    let ratio = count / totals;
+    if recover {
+        if ratio >= RETENTION_RATIO_THRESHOLD {
+            // log::warn!("Bucket {:?} is dirty -> {}", refs.reads, count / totals);
+            let dirt = tmp_dirt;
+
+            let new_passes = recover_from_dirt(
+                dirt,
+                exon_owners,
+                introns_owned,
+                &refs,
+                &mut descriptor,
+                ratio,
+            );
+            new_passes.iter().for_each(|p| {
+                hits.retain(|x| x != p);
+                pass.push(p.to_owned());
+            });
+        }
+    } else {
+        drop(tmp_dirt)
+    }
+
+    // dbg!(&descriptor);
+
+    (hits, pass, blocks, descriptor)
+}
+
+pub fn recover_from_dirt(
+    dirt: Vec<Arc<GenePred>>,
+    exon_owners: BTreeSet<(u64, u64)>,
+    introns_owned: BTreeSet<(u64, u64)>,
+    refs: &RefGenePred,
+    descriptor: &mut HashMap<String, Box<dyn ModuleMap>>,
+    ratio: f32,
+) -> Vec<String> {
+    let mut local_passes = vec![];
+    let mut owner_ratios = HashMap::new();
+    let mut owned_ratios = HashMap::new();
+
+    // exons: 1) build ratios
+    for owner in exon_owners.iter() {
+        let mut background = 0_f32;
+        let mut count = 0_f32;
+        for read in refs.reads.iter() {
+            let ref_exons = &read.exons;
+            let (owner_start, owner_end) = owner;
+
+            if ref_exons.contains(&(*owner_start, *owner_end)) {
+                count += 1.0;
+            }
+
+            // any read that spans the owner is considered background
+            if (read.start < *owner_start) && (*owner_end < read.end) {
+                background += 1.0;
+            }
+        }
+
+        let ratio = count / background;
+        owner_ratios.insert(owner, ratio);
+    }
+
+    // introns: 1) build ratios and 2) remove introns with low ratios
+    let mut supported_introns = vec![];
+    for owned in introns_owned.iter() {
+        let mut background = 0.0;
+        let mut count = 0.0;
+        for read in refs.reads.iter() {
+            let ref_introns = read.get_introns();
+            let (owned_start, owned_end) = owned;
+
+            if ref_introns.contains(&(*owned_start, *owned_end)) {
+                count += 1.0;
+            }
+
+            // any read that spans the owned intron is considered background
+            if (read.start < *owned_start) && (*owned_end < read.end) {
+                background += 1.0;
+            }
+        }
+
+        let ratio = count / background;
+        owned_ratios.insert(owned, ratio);
+
+        if ratio >= INTRON_RETENTION_RECOVERY_THRESHOLD {
+            supported_introns.push(owned);
+        }
+    }
+
+    for read in dirt.clone().iter_mut() {
+        let handle = descriptor.get_mut(&read.name).unwrap();
+
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::IsDirtyComponent),
+                Value::Bool(true),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::ComponentRetentionRatio),
+                serde_json::json!(ratio),
+            )
+            .ok();
+
+        let mut number_of_unsupported_retentions = 0;
+        let mut number_of_supported_retentions = 0;
+        let mut retention_support_ratio = vec![];
+        let mut exon_support_ratio = vec![];
+        let mut is_retention_supported_map = vec![];
+
+        for exon in &read.exons {
+            for intron in introns_owned.iter() {
+                if exon.1 < intron.0 {
+                    break;
+                }
+
+                if exon.0 < intron.0 && exon.1 > intron.1 {
+                    retention_support_ratio.push(
+                        owned_ratios
+                            .get(intron)
+                            .expect("ERROR: Intron not found in intron_owned_ratios"),
+                    );
+                    exon_support_ratio.push(
+                        owner_ratios
+                            .get(exon)
+                            .expect("ERROR: Exon not found in exon_owner_ratios"),
+                    );
+
+                    if supported_introns.contains(&intron) {
+                        // exon eats a 'likely' real intron, fill up the following descriptors:
+                        // unrecover,
+                        // is_retention_supported_map
+                        number_of_supported_retentions += 1;
+                        is_retention_supported_map.push(true);
+                    } else {
+                        // exon eats a 'likely' false intron, fill up the following descriptors:
+                        // recover,
+                        // is_retention_supported_map
+                        number_of_unsupported_retentions += 1;
+                        is_retention_supported_map.push(false);
+                    }
+                }
+            }
+        }
+
+        if number_of_supported_retentions < 1
+            && number_of_unsupported_retentions + number_of_supported_retentions > 0
+        {
+            local_passes.push(read.line().to_owned());
+        } else {
+            handle
+                .set_value(
+                    Box::new(IntronRetentionValue::IsRetentionSupported),
+                    serde_json::json!(true),
+                )
+                .ok();
+        }
+
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::NumberOfUnrecovers),
+                serde_json::json!(number_of_supported_retentions),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::NumberOfRecovers),
+                serde_json::json!(number_of_unsupported_retentions),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::RetentionSupportRatio),
+                serde_json::json!(retention_support_ratio),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::ExonSupportRatio),
+                serde_json::json!(exon_support_ratio),
+            )
+            .ok();
+        handle
+            .set_value(
+                Box::new(IntronRetentionValue::IsRetentionSupportedMap),
+                serde_json::json!(is_retention_supported_map),
+            )
+            .ok();
+    }
+
+    local_passes
 }
 
 #[cfg(test)]
@@ -208,6 +533,7 @@ mod tests {
             threads: 1,
             blacklist: Vec::new(),
             plot: false,
+            recover: false,
         };
 
         assert!(detect_intron_retentions(args).is_ok());
