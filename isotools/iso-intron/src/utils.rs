75cfc4f6321ceb2592ffa8b89a6bdc1c91ffaea4
alejandrogzi <alejandrxgzi@gmail.com>
2024-12-14 01:49:24 +0100

    feat: spliceAI Reader through bigtools!

--- isotools/iso-intron/src/utils.rs
+++ isotools/iso-intron/src/utils.rs
@@ -1,10 +1,15 @@
 use anyhow::Result;
-use config::get_progress_bar;
+use bigtools::{utils::reopen::Reopen, BigWigRead};
+use dashmap::DashMap;
 use hashbrown::{HashMap, HashSet};
 use log::{info, warn};
 use packbed::par_reader;
 use rayon::prelude::*;
 use std::path::PathBuf;
+
+use std::sync::atomic::{AtomicU32, Ordering};
+
+use config::{get_progress_bar, SpliceMap, ACCEPTOR_MINUS, ACCEPTOR_PLUS, DONOR_MINUS, DONOR_PLUS};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Bed4<'a> {
@@ -31,7 +36,7 @@ impl<'a> Bed4<'a> {
 
         Ok(Bed4 {
             chrom,
-            coord: (start, end - 1),
+            coord: (start + 1, end - 1),
             id,
         })
     }
@@ -106,4 +111,64 @@ pub fn parse_bed4<'a>(
             Ok(tracks)
         }
     }
+}
+
+pub fn get_splice_scores<T: AsRef<std::path::Path> + std::fmt::Debug>(dir: T) -> SpliceMap {
+    let plus = vec![
+        dir.as_ref().join(ACCEPTOR_PLUS),
+        dir.as_ref().join(DONOR_PLUS),
+    ];
+    let minus = vec![
+        dir.as_ref().join(ACCEPTOR_MINUS),
+        dir.as_ref().join(DONOR_MINUS),
+    ];
+
+    let (plus, minus) = rayon::join(|| bigwig_to_map(plus), || bigwig_to_map(minus));
+
+    (plus, minus)
+}
+
+fn bigwig_to_map<T: AsRef<std::path::Path> + std::fmt::Debug>(
+    bigwigs: Vec<T>,
+) -> DashMap<String, DashMap<usize, f32>> {
+    let acc = DashMap::new();
+    let count = AtomicU32::new(0);
+
+    for bigwig in bigwigs {
+        let bwread = BigWigRead::open_file(bigwig).unwrap();
+        let chroms: Vec<_> = bwread.chroms().to_vec();
+        let pb = get_progress_bar(
+            chroms.len() as u64,
+            "Parsing BigWigs from one of the strands...",
+        );
+
+        chroms.into_par_iter().for_each(|chr| {
+            let mut bwread = BigWigRead::reopen(&bwread).unwrap();
+
+            let name = chr.name.clone();
+            let length = chr.length;
+            let values = bwread.values(&name, 0, length).unwrap();
+
+            let mapper = DashMap::new();
+
+            values.into_iter().enumerate().for_each(|(i, v)| {
+                if v > 0.0 {
+                    mapper.entry(i).or_insert(v);
+                    count.fetch_add(1, Ordering::Relaxed);
+                }
+            });
+
+            acc.insert(name, mapper);
+            pb.inc(1);
+        });
+
+        pb.finish_and_clear();
+    }
+
+    info!(
+        "Parsed {} splice scores from BigWig!",
+        count.load(Ordering::Relaxed)
+    );
+
+    acc
 }
