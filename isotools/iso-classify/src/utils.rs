a19cd0c9e6ee18ed080466ac683e0bc0e7b1f282
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2025-01-14 17:16:29 +0100

    fix: locate assets path within tree

--- isotools/iso-classify/src/utils.rs
+++ isotools/iso-classify/src/utils.rs
@@ -481,9 +481,14 @@ where
 }
 
 pub fn load_scan_scores() -> Option<(SpliceScoreMap, SpliceScoreMap)> {
-    let assets = std::env::current_dir()
-        .expect("Failed to get executable path")
-        .join(CLASSIFY_ASSETS);
+    let mut assets = std::env::current_dir().expect("Failed to get executable path");
+
+    if !assets.ends_with("iso-classify") {
+        let rest = PathBuf::from("iso-classify").join(CLASSIFY_ASSETS);
+        assets.push(rest);
+    } else {
+        assets = assets.join(CLASSIFY_ASSETS);
+    }
 
     let acceptor_scores = parse_tsv::<AcceptorScores>(
         reader(assets.join(MAXENTSCAN_ACCEPTOR_DB)).expect(
