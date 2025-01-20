43a307a7764e86e0dadc74883c7ac80cbb6ab394
alejandrogzi <alejandrxgzi@gmail.com>
2025-01-20 23:57:03 +0100

    feat: NAG impl!

--- isotools/iso-classify/src/core.rs
+++ isotools/iso-classify/src/core.rs
@@ -1,9 +1,7 @@
 use anyhow::Result;
-use dashmap::DashMap;
-use dashmap::DashSet;
+use dashmap::{DashMap, DashSet};
 use hashbrown::HashSet;
-use packbed::IntronBucket;
-use packbed::{packbed, BedPackage, PackMode};
+use packbed::{packbed, record::IntronPredStats, BedPackage, IntronBucket, PackMode};
 use rayon::prelude::*;
 
 use config::{
@@ -16,6 +14,10 @@ use crate::utils::{
     calculate_acceptor_score, create_splice_map, get_sequences, get_splice_scores,
     load_scan_scores, unpack_blacklist, SpliceScoreMap,
 };
+
+const WINDOW_SIZE: usize = 8;
+const MISMATCHES: u32 = 1;
+const NAG_PATTERNS: [&str; 3] = ["CAG", "TAG", "AAG"];
 
 type ScanScores = Option<(SpliceScoreMap, SpliceScoreMap)>;
 type Genome = DashMap<String, Vec<u8>>;
@@ -108,8 +110,6 @@ fn distribute(
     });
 }
 
-// working with a single component (IntronPred)
-// WARN: This fn should be granularized!
 #[inline(always)]
 fn process_component(
     component: &mut IntronBucket,
@@ -119,9 +119,16 @@ fn process_component(
     genome: &Option<Genome>,
     nag: bool,
 ) -> Vec<String> {
-    let mut acc = Vec::with_capacity(component.introns.len());
+    let chr = component.chrom.clone();
+    let strand = component.strand.clone();
 
-    // get SpliceAi scores
+    let mut acc = if nag {
+        Vec::new()
+    } else {
+        Vec::with_capacity(component.introns.len())
+    };
+
+    // INFO: getting stranded spliceAi scores
     let splice_scores = match component.strand {
         Strand::Forward => Some(&splice_map.0),
         Strand::Reverse => Some(&splice_map.1),
@@ -136,114 +143,20 @@ fn process_component(
             continue;
         }
 
-        // get sequences
-        if let Some(genome) = genome {
-            // depends on the strand
-            match component.strand {
-                Strand::Forward => {
-                    let donor_context = Sequence::new(
-                        genome
-                            .get(component.chrom.as_str())
-                            .expect("ERROR: Could not read donor context!")
-                            [intron_start as usize - 4..intron_start as usize + 5]
-                            .as_ref(),
-                    );
-                    let donor_seq = donor_context.slice(3, 5);
+        get_sj_context(intron, descriptor, &strand, &chr, genome, scan_scores);
+        get_sj_ai_scores(intron, descriptor, splice_scores, &strand);
 
-                    let acceptor_context = Sequence::new(
-                        genome
-                            .get(component.chrom.as_str())
-                            .expect("ERROR: Could not read acceptor context!")
-                            [intron_end as usize - 19..intron_end as usize + 4]
-                            .as_ref(),
-                    );
-                    let acceptor_seq = acceptor_context.slice(18, 20);
-
-                    descriptor.donor_sequence = donor_seq;
-                    descriptor.acceptor_sequence = acceptor_seq;
-                    descriptor.donor_context = donor_context;
-                    descriptor.acceptor_context = acceptor_context;
-                }
-                Strand::Reverse => {
-                    let donor_context = Sequence::new(
-                        genome
-                            .get(component.chrom.as_str())
-                            .expect("ERROR: Could not read donor context!")
-                            [(SCALE - intron_start) as usize - 5
-                                ..(SCALE - intron_start) as usize + 4]
-                            .as_ref(),
-                    )
-                    .reverse_complement();
-                    let donor_seq = donor_context.slice(3, 5);
-
-                    let acceptor_context = Sequence::new(
-                        genome
-                            .get(component.chrom.as_str())
-                            .expect("ERROR: Could not read acceptor context!")
-                            [(SCALE - intron_end) as usize - 4..(SCALE - intron_end) as usize + 19]
-                            .as_ref(),
-                    )
-                    .reverse_complement();
-                    let acceptor_seq = acceptor_context.slice(18, 20);
-
-                    descriptor.donor_sequence = donor_seq;
-                    descriptor.acceptor_sequence = acceptor_seq;
-                    descriptor.donor_context = donor_context;
-                    descriptor.acceptor_context = acceptor_context;
-                }
-            }
-
-            // get MaxEnt scores
-            if let Some(scan_scores) = scan_scores {
-                let (donor_score_map, acceptor_score_map): &(SpliceScoreMap, SpliceScoreMap) =
-                    scan_scores;
-
-                let donor_score = donor_score_map
-                    .get(&descriptor.donor_context)
-                    .and_then(|r| r.get(0))
-                    .unwrap_or(&0.0);
-
-                descriptor.max_ent_donor = *donor_score as f32;
-
-                let acceptor_score =
-                    calculate_acceptor_score(&descriptor.acceptor_context, acceptor_score_map);
-
-                descriptor.max_ent_acceptor = acceptor_score as f32;
-            }
-        }
-
-        // get spliceAi scores
-        if let Some(splice_scores) = splice_scores {
-            if let Some(donor_score_map) = splice_scores.0.as_ref() {
-                let acceptor_score_map = splice_scores
-                    .1
-                    .as_ref()
-                    .expect("ERROR: Acceptor score map is None, this is a bug!");
-
-                let (intron_donor, intron_acceptor) = match component.strand {
-                    // donor [-1 to match bigtools coords]
-                    Strand::Forward => (intron_start as usize - 1, intron_end as usize),
-                    // acceptor [-1 to match bigtools coords]
-                    Strand::Reverse => (
-                        (SCALE - intron_start) as usize,
-                        (SCALE - intron_end) as usize - 1,
-                    ),
-                };
-
-                let (donor_score, acceptor_score) = (
-                    donor_score_map
-                        .get(&intron_donor)
-                        .map(|r| *r)
-                        .unwrap_or(0.0),
-                    acceptor_score_map
-                        .get(&intron_acceptor)
-                        .map(|r| *r)
-                        .unwrap_or(0.0),
-                );
-
-                descriptor.splice_ai_donor = donor_score;
-                descriptor.splice_ai_acceptor = acceptor_score;
-            }
+        if nag && descriptor.acceptor_sequence == "AG" {
+            scan_nag_repeats(
+                intron,
+                descriptor,
+                &strand,
+                &chr,
+                genome,
+                scan_scores,
+                splice_scores,
+                &mut acc,
+            );
         }
 
         let (intron_start, intron_end) = match component.strand {
@@ -251,13 +164,300 @@ fn process_component(
             Strand::Reverse => ((SCALE - intron_end), (SCALE - intron_start)),
         };
 
-        acc.push(descriptor.fmt(
-            component.chrom.clone(),
-            component.strand.clone(),
-            intron_start,
-            intron_end,
-        ));
+        acc.push(descriptor.fmt(&chr, &strand, intron_start, intron_end));
     }
 
     acc
+}
+
+fn get_sj_context(
+    intron: &(u64, u64),
+    descriptor: &mut IntronPredStats,
+    strand: &Strand,
+    chr: &String,
+    genome: &Option<Genome>,
+    scan_scores: &ScanScores,
+) {
+    let intron_start = intron.0 as u64;
+    let intron_end = intron.1 as u64;
+
+    if let Some(genome) = genome {
+        match strand {
+            Strand::Forward => {
+                // INFO: For TOGA nag we need +3 upstream and +3 downstream
+                // INFO: For RT repeats we need 12-mers (11 exon + 1 intron) at 5'
+                // INFO: and 11 intron + 1 exon at 3'
+
+                let donor_context = Sequence::new(
+                    genome
+                        .get(chr)
+                        .expect("ERROR: Could not read donor context!")
+                        [intron_start as usize - 12..intron_start as usize + 5]
+                        .as_ref(),
+                );
+                let donor_seq = donor_context.slice(11, 13);
+                let donor_rt_context = donor_context.slice(0, 12);
+
+                let acceptor_context = Sequence::new(
+                    genome
+                        .get(chr)
+                        .expect("ERROR: Could not read acceptor context!")
+                        [intron_end as usize - 19..intron_end as usize + 4]
+                        .as_ref(),
+                );
+                let acceptor_seq = acceptor_context.slice(18, 20);
+                let acceptor_rt_context = acceptor_context.slice(8, 21);
+
+                descriptor.donor_sequence = donor_seq;
+                descriptor.donor_context = donor_context;
+
+                descriptor.acceptor_sequence = acceptor_seq;
+                descriptor.acceptor_context = acceptor_context;
+
+                descriptor.donor_rt_context = donor_rt_context;
+                descriptor.acceptor_rt_context = acceptor_rt_context;
+            }
+            Strand::Reverse => {
+                let donor_context = Sequence::new(
+                    genome
+                        .get(chr)
+                        .expect("ERROR: Could not read donor context!")
+                        [(SCALE - intron_start) as usize - 5..(SCALE - intron_start) as usize + 12]
+                        .as_ref(),
+                )
+                .reverse_complement();
+                let donor_seq = donor_context.slice(11, 13);
+                let donor_rt_context = donor_context.slice(0, 12);
+
+                let acceptor_context = Sequence::new(
+                    genome
+                        .get(chr)
+                        .expect("ERROR: Could not read acceptor context!")
+                        [(SCALE - intron_end) as usize - 4..(SCALE - intron_end) as usize + 19]
+                        .as_ref(),
+                )
+                .reverse_complement();
+                let acceptor_seq = acceptor_context.slice(18, 20);
+                let acceptor_rt_context = acceptor_context.slice(8, 21);
+
+                descriptor.donor_sequence = donor_seq;
+                descriptor.donor_context = donor_context;
+
+                descriptor.acceptor_sequence = acceptor_seq;
+                descriptor.acceptor_context = acceptor_context;
+
+                descriptor.donor_rt_context = donor_rt_context;
+                descriptor.acceptor_rt_context = acceptor_rt_context;
+            }
+        }
+
+        get_sj_max_entropy(descriptor, scan_scores);
+        scan_rt_repeats(descriptor);
+    }
+}
+
+fn get_sj_max_entropy(descriptor: &mut IntronPredStats, scan_scores: &ScanScores) {
+    // get MaxEnt scores
+    if let Some(scan_scores) = scan_scores {
+        let (donor_score_map, acceptor_score_map): &(SpliceScoreMap, SpliceScoreMap) = scan_scores;
+
+        let donor_max_ent_context = &descriptor.donor_context.slice(8, 17);
+
+        if donor_max_ent_context.len() != 9 {
+            eprintln!("ERROR: Donor context is not 9 bases long!");
+        }
+
+        let donor_score = donor_score_map
+            .get(donor_max_ent_context)
+            .and_then(|r| r.get(0))
+            .unwrap_or(&0.0);
+
+        descriptor.max_ent_donor = *donor_score as f32;
+
+        let acceptor_score =
+            calculate_acceptor_score(&descriptor.acceptor_context, acceptor_score_map);
+
+        descriptor.max_ent_acceptor = acceptor_score as f32;
+    }
+}
+
+fn get_sj_ai_scores(
+    intron: &(u64, u64),
+    descriptor: &mut IntronPredStats,
+    splice_scores: Option<&SharedSpliceMap>,
+    strand: &Strand,
+) {
+    let intron_start = intron.0 as u64;
+    let intron_end = intron.1 as u64;
+
+    // get spliceAi scores
+    if let Some(splice_scores) = splice_scores {
+        if let Some(donor_score_map) = splice_scores.0.as_ref() {
+            let acceptor_score_map = splice_scores
+                .1
+                .as_ref()
+                .expect("ERROR: Acceptor score map is None, this is a bug!");
+
+            let (intron_donor, intron_acceptor) = match strand {
+                // donor [-1 to match bigtools coords]
+                Strand::Forward => (intron_start as usize - 1, intron_end as usize),
+                // acceptor [-1 to match bigtools coords]
+                Strand::Reverse => (
+                    (SCALE - intron_start) as usize,
+                    (SCALE - intron_end) as usize - 1,
+                ),
+            };
+
+            let (donor_score, acceptor_score) = (
+                donor_score_map
+                    .get(&intron_donor)
+                    .map(|r| *r)
+                    .unwrap_or(0.0),
+                acceptor_score_map
+                    .get(&intron_acceptor)
+                    .map(|r| *r)
+                    .unwrap_or(0.0),
+            );
+
+            descriptor.splice_ai_donor = donor_score;
+            descriptor.splice_ai_acceptor = acceptor_score;
+        }
+    }
+}
+
+fn process_nag_pattern(
+    base_intron: &(u64, u64),
+    offset: i64,
+    strand: &Strand,
+    chr: &String,
+    genome: &Option<Genome>,
+    scan_scores: &ScanScores,
+    splice_scores: Option<&SharedSpliceMap>,
+) -> String {
+    let intron = (base_intron.0 as u64, (base_intron.1 as i64 + offset) as u64);
+
+    let mut new_descriptor = IntronPredStats::new();
+    get_sj_context(
+        &intron,
+        &mut new_descriptor,
+        strand,
+        chr,
+        genome,
+        scan_scores,
+    );
+    get_sj_ai_scores(&intron, &mut new_descriptor, splice_scores, strand);
+    new_descriptor.is_nag_intron = true;
+
+    let scaled_intron = match strand {
+        Strand::Forward => intron,
+        Strand::Reverse => ((SCALE - intron.1), (SCALE - intron.0)),
+    };
+
+    new_descriptor.fmt(chr, strand, scaled_intron.0, scaled_intron.1)
+}
+
+fn scan_nag_repeats(
+    intron: &(u64, u64),
+    descriptor: &mut IntronPredStats,
+    strand: &Strand,
+    chr: &String,
+    genome: &Option<Genome>,
+    scan_scores: &ScanScores,
+    splice_scores: Option<&SharedSpliceMap>,
+    acc: &mut Vec<String>,
+) {
+    let pre_acceptor = &descriptor.acceptor_context.slice(14, 17);
+    let post_acceptor = &descriptor.acceptor_context.slice(20, 23);
+
+    if NAG_PATTERNS.contains(&pre_acceptor.as_str()) {
+        acc.push(process_nag_pattern(
+            intron,
+            -3,
+            strand,
+            chr,
+            genome,
+            scan_scores,
+            splice_scores,
+        ));
+    }
+
+    if NAG_PATTERNS.contains(&post_acceptor.as_str()) {
+        acc.push(process_nag_pattern(
+            intron,
+            3,
+            strand,
+            chr,
+            genome,
+            scan_scores,
+            splice_scores,
+        ));
+    }
+}
+
+fn scan_rt_repeats(descriptor: &mut IntronPredStats) {
+    unsafe {
+        scan_sequence(descriptor);
+    }
+}
+
+#[inline(always)]
+unsafe fn scan_sequence(descriptor: &mut IntronPredStats) {
+    let donor = &descriptor.donor_rt_context.as_bytes();
+    let acceptor = &descriptor.acceptor_rt_context.as_bytes();
+
+    #[inline(always)]
+    fn base_to_bits(base: u8) -> u8 {
+        match base {
+            b'A' => 0,
+            b'C' => 1,
+            b'G' => 2,
+            b'T' => 3,
+            b'N' => 0,
+            _ => panic!("Invalid base"),
+        }
+    }
+
+    // INFO: convert 8-mer to u16 (2 bits per base = 16 bits total)
+    #[inline(always)]
+    fn window_to_int(window: &[u8]) -> u16 {
+        let mut value: u16 = 0;
+        for &base in window {
+            value = (value << 2) | (base_to_bits(base) as u16);
+        }
+        value
+    }
+
+    #[inline(always)]
+    fn hamming_distance(a: u16, b: u16) -> u32 {
+        (a ^ b).count_ones()
+    }
+
+    if donor.len() < WINDOW_SIZE || acceptor.len() < WINDOW_SIZE {
+        return;
+    }
+
+    // INFO: build a set of all 8-mers in seq1
+    let mut kmers = HashSet::with_capacity(donor.len() - WINDOW_SIZE + 1);
+    for idx in 0..=donor.len() - WINDOW_SIZE {
+        let kmer = window_to_int(&donor[idx..idx + WINDOW_SIZE]);
+        kmers.insert((kmer, idx));
+    }
+
+    // INFO: compare k-mers from seq2 against the set
+    for idx in 0..=acceptor.len() - WINDOW_SIZE {
+        let kmer2 = window_to_int(&acceptor[idx..idx + WINDOW_SIZE]);
+
+        for &(kmer1, _) in &kmers {
+            // INFO: 2 bits difference = 1 base mismatch
+            if hamming_distance(kmer1, kmer2) <= MISMATCHES + 1 {
+                // INFO: returning bool and skipping early
+                // repeats.push((i, j));
+                // dbg!("Match found: seq1[{}..{}], seq2[{}..{}]", _, _ + 8, idx, idx + 8);
+                descriptor.is_rt_intron = true;
+                return;
+            }
+        }
+    }
+
+    descriptor.is_rt_intron = false;
 }
