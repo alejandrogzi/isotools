82618a17e79fd6ebaed1073d8fb2df7aace14633
alejandrogzi <alejandrxgzi@gmail.com>
2025-10-01 00:24:22 +0200

    chore: dropping excesive docs

--- isotools/iso-orphan/src/lib.rs
+++ isotools/iso-orphan/src/lib.rs
@@ -19,70 +19,18 @@
 //! iso-orphan <ARGUMENTS>
 //! ```
 //!
-//! ## Arguments
-//!
-//! - `-b`, `--bed`: Paths to BED12 files delimited by comma
-//!     - Required: true
-//!     - Value name: PATHS
-//!     - Value delimiter: comma
-//!     - Number of values: 1..
-//!     - Help: Paths to BED12 files delimited by comma
-//! - `-t`, `--toga`: Paths to TOGA BED12 files delimited by comma
-//!     - Required: false
-//!     - Value name: PATHS
-//!     - Value delimiter: comma
-//!     - Number of values: 1..
-//!     - Help: Paths to TOGA BED12 files delimited by comma
-//! - `-A`, `--all`: Complete mode, accounting for overlapping and non-overlapping reads (default: true)
-//!     - Required: false
-//!     - Value name: FLAG
-//!     - Help: Complete mode, accounting for overlapping and non-overlapping reads (default: true)
-//! - `-O`, `--overlapping`: Overlapping mode, accounting for reads that overlap a TOGA reference (requires TOGA arg)
-//!     - Required: false
-//!     - Value name: FLAG
-//!     - Help: Overlapping mode, accounting for reads that overlap a TOGA reference (requires TOGA arg)
-//! - `-N`, `--non-overlapping`: Non-overlapping mode, triggers self-guided algorithm to discard orphans
-//!     - Required: false
-//!     - Value name: FLAG
-//!     - Help: Non-overlapping mode, triggers self-guided algorithm to discard orphans
-//! - `-K`, `--keep`: Writes orphan reads to a separate file (default: false)
-//!     - Required: false
-//!     - Value name: FLAG
-//!     - Help: Writes orphan reads to a separate file (default: false)
-//! - `-m`, `--min-read-num`: Threshold for minimum number of reads to be considered a component when self-guided (default: 5)
-//!     - Required: false
-//!     - Value name: NUM
-//!     - Default value: 5
-//!     - Help: Threshold for minimum number of reads to be considered a component when self-guided (default: 5)
-//! - `-o`, `--outdir`: Path to output directory (/orphans)
-//!     - Required: false
-//!     - Default value: .
-//!     - Help: Path to output directory (/orphans)
-//! - `-n`, `--name`: Name of the output file without any extension
-//!     - Required: false
-//!     - Default value: output
-//!     - Help: Name of the output file without any extension
-//! - `-t`, `--threads`: Number of threads
-//!     - Required: false
-//!     - Default value: num_cpus::get()
-//!     - Help: Number of threads
-//! - `-L`, `--level`: Logging level
-//!     - Required: false
-//!     - Default value: info
-//!     - Help: Logging level
-//!
 //! ## Examples
 //!
 //! ```bash
-//! iso-orphan --bed tests/data/mm39.bed --toga tests/data/mm39.toga.bed --outdir tests/data --name test --threads 1
+//! iso-orphan --bed tests/data/mm39.bed --toga tests/data/mm39.toga.bed --all --outdir tests/data --name test --threads 1
 //! ```
 //!
 //! ```bash
-//! iso-orphan --bed tests/data/mm39.bed --all --overlapping --outdir tests/data --name test --threads 1
+//! iso-orphan --bed tests/data/mm39.bed --all --outdir tests/data --name test --threads 1
 //! ```
 //!
 //! ```bash
-//!  iso-orphan -b tests/data/mm39.bed -t tests/data/mm39.toga.bed -A -O -N test -T 1
+//!  iso-orphan -b tests/data/mm39.bed -t tests/data/mm39.toga.bed -O test -T 1
 //! ```
 
 pub mod cli;
